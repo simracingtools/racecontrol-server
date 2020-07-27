f7301dad0dade8316489154f8335e275c3d868be
Robert Bausdorf <r.bausdorf@clevershuttle.de>
2020-07-27 14:26:20 +0200

    Initial

---
+++ src/main/java/de/bausdorf/simracing/racecontrol/RacecontrolServerApplication.java
@@ -0,0 +1,13 @@
+package de.bausdorf.simracing.racecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacecontrolServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacecontrolServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/bausdorf/simracing/racecontrol/RacecontrolServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bausdorf.simracing.racecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacecontrolServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
